a7dd1421b3f48d434238ed570e0b9d1a009aa1d1
Ohhaeseo <dhgotj@naver.com>
2024-03-27 14:36:18 +0900

    LOGO4

---
+++ js/Basic_js_test.js
@@ -0,0 +1,20 @@
+var jb = 'hi'; 
+var a = 1;
+var b;
+b = 5;
+
+if(true){
+    let c = 'let 접근';
+    var c_1 = 'var 접근';
+}
+//console.log(c);// Error?
+console.log(c_1);
+
+let d = 5;
+//let d = '값 재할당';// Error?
+console.log(d);
+
+const e = '상수1 접근';
+//e = 5;
+//const f//
+console.log(e);
---
+++ js/search.js
@@ -0,0 +1,5 @@
+document.getElementById("search_btn").addEventListener('click', search_message);
+function search_message(){
+    alert("검색을 수행합니다!");
+    }
+    
